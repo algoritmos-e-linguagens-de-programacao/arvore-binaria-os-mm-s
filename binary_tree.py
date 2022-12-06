accd0447b101d33d31c75b39137faf16fa69812f
Laboratório Informática <labinfo@unifil.br>
2022-12-05 21:14:57 -0300

    Finalizado

--- binary_tree.py
+++ binary_tree.py
@@ -2,13 +2,14 @@ from node import Node
 
 COUNT = [10]
 
+
 class BinaryTree:
 
   def __init__(self, data=None):
     self.root = None
 
   # ==================== ADICIONAR ===============================
-  
+
   def adicionar(self, value):
     node = Node(value)
     no = None
@@ -46,8 +47,8 @@ class BinaryTree:
   # =================== REMOVER ===================
 
   def remove(self, value):
-     print("O valor " + str(value) + " foi removido")
-     return self._remove(value, self.root)
+    print("O valor " + str(value) + " foi removido")
+    return self._remove(value, self.root)
 
   def _remove(self, value, node):
     if value < node.data:
@@ -60,48 +61,82 @@ class BinaryTree:
       elif node.right is None:
         return node.left
       else:
+
         def _menorFolha(node=None):
           while node.left:
             node = node.left
           return node.data
+
         organize = _menorFolha(node.right)
         node.data = organize
         node.right = self._remove(organize, node.right)
     return node
 
   # ======================= IMPRESSÃO ESTRUTURA DA ÁRVORE GRAFICO ========================================
-  
-  def height(self,root):
+
+  def height(self, root):
     if root is None:
-        return 0
-    return max(self.height(root.left), self.height(root.right))+1
- 
- 
-  def getcol(self,h):
-      if h == 1:
-          return 1
-      return self.getcol(h-1) + self.getcol(h-1) + 1
-   
-   
-  def printTree(self,M, root, col, row, height):
-      if root is None:
-          return
-      M[row][col] = root.data
-      self.printTree(M, root.left, col-pow(2, height-2), row+1, height-1)
-      self.printTree(M, root.right, col+pow(2, height-2), row+1, height-1)
-   
-   
+      return 0
+    return max(self.height(root.left), self.height(root.right)) + 1
+
+  def getcol(self, h):
+    if h == 1:
+      return 1
+    return self.getcol(h - 1) + self.getcol(h - 1) + 1
+
+  def printTree(self, M, root, col, row, height):
+    if root is None:
+      return
+    M[row][col] = root.data
+    self.printTree(M, root.left, col - pow(2, height - 2), row + 1, height - 1)
+    self.printTree(M, root.right, col + pow(2, height - 2), row + 1,
+                   height - 1)
+
   def TreePrinter(self):
-      h = self.height(self.root)
-      col = self.getcol(h)
-      M = [[0 for _ in range(col)] for __ in range(h)]
-      self.printTree(M, self.root, col//2, 0, h)
-      for i in M:
-          for j in i:
-              if j == 0:
-                  print(".", end=" ")
-              else:
-                  print(j, end=" ")
-          print("")
+    h = self.height(self.root)
+    col = self.getcol(h)
+    M = [[0 for _ in range(col)] for __ in range(h)]
+    self.printTree(M, self.root, col // 2, 0, h)
+    for i in M:
+      for j in i:
+        if j == 0:
+          print(".", end=" ")
+        else:
+          print(j, end=" ")
+      print("")
 
   # ============================================================================================
+
+  def preOrdem(self):
+    print("Pre Ordem")
+    self._preOrdem(self.root)
+    
+  def emOrdem(self):
+    print("Em Ordem")
+    self._emOrdem(self.root)
+
+  def posOrdem(self):
+    print("Pos Ordem")
+    self._posOrdem(self.root)
+
+  def _emOrdem(self, node):
+    if node.left:
+      self._emOrdem(node.left)
+    print(node)
+    if node.right:
+      self._emOrdem(node.right)
+
+  def _preOrdem(self, node):
+    print(node)
+    if node.left:
+      self._preOrdem(node.left)
+    if node.right:
+      self._preOrdem(node.right)
+
+  def _posOrdem(self, node):
+    if node.left:
+      self._posOrdem(node.left)
+    if node.right:
+      self._posOrdem(node.right)
+    print(node)
+
--- main.py
+++ main.py
@@ -2,30 +2,36 @@ from binary_tree import BinaryTree
 
 arvore = BinaryTree()
 
-arvore.adicionar(9)
+arvore.adicionar(5)
 
+arvore.adicionar(3)
 arvore.adicionar(2)
-arvore.adicionar(6)
-arvore.adicionar(3)
-
 arvore.adicionar(8)
-arvore.adicionar(321)
-arvore.adicionar(1)
+arvore.adicionar(4)
+arvore.adicionar(50)
+
+arvore.adicionar(6)
+arvore.adicionar(7)
 arvore.adicionar(3)
 
 print("")
 
-arvore.search(9)
-arvore.TreePrinter()
-
-
-print("")
-
-
-arvore.remove(9)
+arvore.search(8)
 arvore.TreePrinter()
 
 print("")
 
 arvore.remove(6)
 arvore.TreePrinter()
+
+print("")
+
+arvore.preOrdem()
+
+print("")
+
+arvore.emOrdem()
+
+print("")
+
+arvore.posOrdem()
